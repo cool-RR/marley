92f3bf221c5b593d8b2d22ed06a09aa705526402
Ram Rachum <ram@rachum.com>
2020-10-14 20:42:48 +0300

    Adding tests

---
+++ tests/test_gamey/test_grid_royale.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Ram Rachum and collaborators.
+# This program is distributed under the MIT license.
+
+import grid_royale
+from grid_royale import State, Observation, Action, Culture
+from grid_royale import gamey
+
+def test_simple():
+    culture = Culture(n_players=1, board_size=4)
+    state_0 = culture.make_initial()
+    (letter,) = culture.player_id_to_strategy
+    assert isinstance(state_0, State)
+    assert state_0.board_size == 4
+    assert not state_0.is_end
+    assert not state_0.bullets
+    state_1 = state_0.get_next_state_from_actions({letter: Action.shoot_right})
+    state_2 = state_1.get_next_state_from_actions({letter: Action.shoot_left})
+    assert state_1.bullets or state_2.bullets
+
+
