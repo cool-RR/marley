43ef6eaaef71d2e6675ba0159bdac4915a160dd0
Ram Rachum <ram@rachum.com>
2021-09-25 17:04:46 +0300

    Fix static assets

--- marley/__init__.py
+++ marley/__init__.py
@@ -22,7 +22,7 @@ from . import sharknado # todo remove
 __VersionInfo = collections.namedtuple('VersionInfo',
                                        ('major', 'minor', 'micro'))
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __version_info__ = __VersionInfo(*(map(int, __version__.split('.'))))
 
 def import_all_worlds():
